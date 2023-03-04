e1a271f538117adacf2c247247e8546029fbb4ea
Dhruv Kapadia <dhruvkapadia01@hotmail.com>
2023-03-04 17:44:30 -0500

    Fix getComputerChoice function

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 
 let max = 3;
 let min = 1;
-let selection = Math.random() * (((max - min) + 1) + min) ;
-
+let selection = Math.floor((Math.random() * (((max - min) + 1)) + min)) ;
+console.log(selection);
 
 
 
