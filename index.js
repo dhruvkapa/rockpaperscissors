634f22e2caded3c821751186df8957a10a357b0d
Dhruv Kapadia <dhruvkapadia01@hotmail.com>
2023-03-04 17:24:56 -0500

    Add the getComputerChoice function

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello world!")
+
+// max = 3
+// min is 1
+
+let max = 3;
+let min = 1;
+let selection = Math.random() * (((max - min) + 1) + min) ;
+
+
+
+
+function getComputerChoice(){
+    if(selection == 1){
+        return "Rock";
+    } else if (selection == 2){
+        return "Paper";
+    } else if(selection == 3){
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
