88f2b16d2c03615215d3c90e6eb511c661af90a6
Dhruv Kapadia <dhruvkapadia01@hotmail.com>
2023-03-04 18:29:29 -0500

    Add scores to game

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-
-// max = 3
-// min is 1
-
 let max = 3;
 let min = 1;
 let selection = Math.floor((Math.random() * (((max - min) + 1)) + min)) ;
 console.log(selection);
 
-
-
+let scorePlayer = 0;
+let scoreComputer = 0;
+let ties = 0;
 function getComputerChoice(){
     if(selection == 1){
         return "Rock";
@@ -22,26 +19,38 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
     if(playerSelection.toLowerCase() == "rock"){
         if(computerSelection.toLowerCase() == "rock"){
+            ties +=1;
             console.log("Tie!");
         } else if (computerSelection.toLowerCase() == "paper"){
+            scoreComputer += 1;
             console.log("Computer wins!");
         } else if (computerSelection.toLowerCase() == "scissors"){
+            scorePlayer += 1;
             console.log("Player wins!");
         }
     } else if (playerSelection.toLowerCase() == "paper"){
         if(computerSelection.toLowerCase() == "rock"){
+            scorePlayer += 1;
+
             console.log("Player Wins!");
         } else if (computerSelection.toLowerCase() == "paper"){
             console.log("Tie!");
+            ties += 1;
         } else if (computerSelection.toLowerCase() == "scissors"){
+            scoreComputer += 1;
+
             console.log("Computer wins!");
         }
     } else if(playerSelection.toLowerCase() == "scissors"){
         if(computerSelection.toLowerCase() == "rock"){
+            scoreComputer += 1;
             console.log("Computer Wins!");
         } else if (computerSelection.toLowerCase() == "paper"){
+            scorePlayer += 1;
+
             console.log("Player wins!");
         } else if (computerSelection.toLowerCase() == "scissors"){
+            ties +=1;
             console.log("Tie!");
         }
     }
@@ -63,3 +72,4 @@ while(i < 5){
     i++;
 }
 
+console.log("Player Score: " + scorePlayer + " Computer Score: " + scoreComputer + " Ties: " + ties);
